163e08edd3141ae2abdeac6a21348d1b83c094a2
Raid Thabet <thabet.raid123@gmail.com>
2025-04-20 13:29:47 +0100

    Created Task entity

---
+++ src/main/java/com/raid/tasks/domain/entities/Task.java
@@ -0,0 +1,75 @@
+package com.raid.tasks.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tasks")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description")
+    private String description; // optional
+
+    @Column(name = "due_date")
+    private LocalDateTime dueDate; // optional
+
+    @Column(name = "status", nullable = false)
+    private TaskStatus status;
+
+    @Column(name = "priority", nullable = false)
+    private TaskPriority priority;
+
+    @Column(name = "created", nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime created;
+
+    @Column(name = "updated", nullable = false)
+    @UpdateTimestamp
+    private LocalDateTime updated;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return Objects.equals(id, task.id) && Objects.equals(title, task.title) && Objects.equals(description, task.description) && Objects.equals(dueDate, task.dueDate) && status == task.status && priority == task.priority && Objects.equals(created, task.created) && Objects.equals(updated, task.updated);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, description, dueDate, status, priority, created, updated);
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", dueDate=" + dueDate +
+                ", status=" + status +
+                ", priority=" + priority +
+                ", created=" + created +
+                ", updated=" + updated +
+                '}';
+    }
+}
---
+++ src/main/java/com/raid/tasks/domain/entities/TaskPriority.java
@@ -0,0 +1,5 @@
+package com.raid.tasks.domain.entities;
+
+public enum TaskPriority {
+    HIGH, MEDIUM, LOW
+}
